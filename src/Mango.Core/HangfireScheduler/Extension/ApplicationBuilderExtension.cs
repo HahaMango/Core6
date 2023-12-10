841580ea693229af350d501ec6871e8935206a1f
Chiva Chen <q932104843@outlook.com>
2023-12-10 20:37:27 +0800

    增加hangfire支持

---
+++ src/Mango.Core/HangfireScheduler/Extension/ApplicationBuilderExtension.cs
@@ -0,0 +1,62 @@
+﻿using Hangfire.Dashboard.BasicAuthorization;
+using Hangfire;
+using Microsoft.AspNetCore.Builder;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Hangfire.HttpJob.Agent.MysqlConsole;
+
+namespace Mango.Core.HangfireScheduler.Extension
+{
+    public static class ApplicationBuilderExtension
+    {
+        /// <summary>
+        /// 添加hangfire面板
+        /// </summary>
+        /// <param name="app"></param>
+        /// <param name="loginName"></param>
+        /// <param name="password"></param>
+        /// <returns></returns>
+        public static IApplicationBuilder UseMangoHangfireDashboard(this IApplicationBuilder app, string loginName, string password)
+        {
+            Thread.CurrentThread.CurrentUICulture = new System.Globalization.CultureInfo("zh-CN");
+            app.UseHangfireDashboard(options: new DashboardOptions
+            {
+                Authorization = new[]
+                {
+                    new BasicAuthAuthorizationFilter(new BasicAuthAuthorizationFilterOptions
+                    {
+                        SslRedirect = false,
+                        RequireSsl = false,
+                        LoginCaseSensitive = true,
+                        Users = new []
+                        {
+                            new BasicAuthAuthorizationUser
+                            {
+                                Login = loginName,
+                                PasswordClear = password
+                            }
+                        }
+                    })
+                }
+            });
+            return app;
+        }
+
+        public static IApplicationBuilder UseMangoHangfireAgent(this IApplicationBuilder app, string authName, string password)
+        {
+            app.UseHangfireJobAgent(op =>
+            {
+                op.Enabled(true);
+                op.WithSitemap("/jobagent");
+                op.EnabledBasicAuth(true);
+                op.WithBasicUserName(authName);
+                op.WithBasicUserPwd(password);
+            });
+            return app;
+        }
+    }
+}
---
+++ src/Mango.Core/HangfireScheduler/Extension/ServiceCollectionExtension.cs
@@ -0,0 +1,71 @@
+﻿using Hangfire;
+using Hangfire.HttpJob;
+using Hangfire.HttpJob.Agent.MysqlConsole;
+using Hangfire.MySql;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyModel;
+using System.Reflection;
+using System.Transactions;
+using MySqlStorageOptions = Hangfire.MySql.MySqlStorageOptions;
+
+namespace Mango.Core.HangfireScheduler.Extension
+{
+    public static class ServiceCollectionExtension
+    {
+        /// <summary>
+        /// 添加Hangfire服务端
+        /// </summary>
+        /// <param name="services"></param>
+        /// <param name="connectionString"></param>
+        /// <returns></returns>
+        public static IServiceCollection AddMangoHangfire(this IServiceCollection services, string connectionString)
+        {
+            services.AddHangfire(cf =>
+            {
+                cf.SetDataCompatibilityLevel(CompatibilityLevel.Version_180);
+                cf.UseSimpleAssemblyNameTypeSerializer();
+                cf.UseRecommendedSerializerSettings();
+                cf.UseStorage(new MySqlStorage(connectionString, new MySqlStorageOptions
+                {
+                    TransactionIsolationLevel = IsolationLevel.ReadCommitted,
+                    QueuePollInterval = TimeSpan.FromSeconds(15),
+                    JobExpirationCheckInterval = TimeSpan.FromHours(1),
+                    CountersAggregateInterval = TimeSpan.FromMinutes(5),
+                    PrepareSchemaIfNecessary = false,
+                    DashboardJobListLimit = 50000,
+                    TransactionTimeout = TimeSpan.FromMinutes(1),
+                }));
+                cf.UseHangfireHttpJob();
+            });
+            //服务端
+            services.AddHangfireServer();
+            return services;
+        }
+
+        /// <summary>
+        /// 添加Hangfire Agent
+        /// </summary>
+        /// <param name="services"></param>
+        /// <returns></returns>
+        public static IServiceCollection AddMangoHangfireAgent(this IServiceCollection services)
+        {
+            services.AddHangfireJobAgent(op =>
+            {
+                var result = new List<Assembly>();
+                var assemblies = DependencyContext.Default.CompileLibraries
+                    .Where(item => item.Name.StartsWith("Mango"))
+                    .ToList();
+                foreach (var assembly in assemblies)
+                {
+                    result.Add(Assembly.Load(assembly.Name));
+                }
+
+                foreach (var assembly in result)
+                {
+                    op.AddJobAgent(assembly);
+                }
+            });
+            return services;
+        }
+    }
+}
